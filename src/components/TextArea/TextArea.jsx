96b7ae434cd93520d0cf686fb86f7efe9ae28db3
First_Encounter <needenergy27@gmail.com>
2024-09-27 23:07:17 +0700

    add small refactoring and connect to modal window

--- src/components/TextArea/TextArea.jsx
+++ src/components/TextArea/TextArea.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import { Modal } from "../Modal/Modal"; 
+
 import "./TextArea.css";
 
 function TextArea() {
   const [textAreaValue, setTextAreaValue] = React.useState("");
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
 
   const TextAreaClear = () => {
     setTextAreaValue("");
@@ -38,27 +41,30 @@ function TextArea() {
   };
 
   return (
-    <div className="text-area-wrapper">
-      <label htmlFor="text-area" className="label-text-area">
-        Вставьте JSON в поле
-      </label>
-      <textarea
-        id="text-area"
-        placeholder='{"key":"value}'
-        value={textAreaValue}
-        onChange={inputHandler}
-      ></textarea>
-      <div className="main-buttons-wrapper">
-        <button className="buttons">Get</button>
-        <button className="buttons" onClick={saveASHandler}>
-          Save as
-        </button>
-        <button className="buttons" onClick={TextAreaClear}>
-          Clear
-        </button>
-        <button className="buttons">Decode</button>
+    <>
+      <div className="text-area-wrapper">
+        <label htmlFor="text-area" className="label-text-area">
+          Вставьте JSON в поле
+        </label>
+        <textarea
+          id="text-area"
+          placeholder='{"key":"value}'
+          value={textAreaValue}
+          onChange={inputHandler}
+        ></textarea>
+        <div className="main-buttons-wrapper">
+          <button className="buttons" onClick={() => setIsModalOpen(true)}>Get</button>
+          <button className="buttons" onClick={saveASHandler}>
+            Save as
+          </button>
+          <button className="buttons" onClick={TextAreaClear}>
+            Clear
+          </button>
+          <button className="buttons">Decode</button>
+        </div>
       </div>
-    </div>
+      <Modal isOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+    </>
   );
 }
 
