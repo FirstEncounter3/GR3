4d3c51f6e98314adb214715abe48028e2b328e64
First_Encounter <needenergy27@gmail.com>
2024-09-27 23:56:37 +0700

    add luminous indicator

--- src/components/InfoBoard/InfoBoard.jsx
+++ src/components/InfoBoard/InfoBoard.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 import './InfoBoard.css'
 
 function InfoBoard() {
-    const color = '#28B463'
+  const [dots, setDots] = useState('...')
+  const [color, setColor] = useState('#28B463')
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+        setDots(prevDots => {
+            if (prevDots.length < 3) {
+                return prevDots + '.';
+            } else {
+                return '';
+            }
+        });
+        setColor(prevColor => {
+            if (prevColor === '#28B463') {
+                return '#4F75FF';
+            } else {
+                return '#28B463';
+            }
+        });
+    }, 700);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <span className='info-board'><span style={{color: color}}>●</span> Listen...</span>
+    <span className='info-board'><span style={{color: color}}>●</span> Listen{dots}</span>
   )
 }
 
