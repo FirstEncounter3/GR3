cd387450f3fb44b62b49025a705bec82796828ee
First_Encounter <needenergy27@gmail.com>
2024-09-28 01:27:59 +0700

    add toasts for info, json tree display, decode func, save as func

--- src/components/TextArea/TextArea.jsx
+++ src/components/TextArea/TextArea.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { JSONTree } from "react-json-tree";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { Modal } from "../Modal/Modal"; 
 
@@ -7,9 +10,11 @@ import "./TextArea.css";
 function TextArea() {
   const [textAreaValue, setTextAreaValue] = React.useState("");
   const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [formattedJson, setFormattedJson] = React.useState(null);
 
   const TextAreaClear = () => {
     setTextAreaValue("");
+    setFormattedJson(null);
   };
 
   const saveASHandler = async () => {
@@ -28,17 +33,42 @@ function TextArea() {
         });
 
         const writableStream = await fileHandle.createWritable();
-        await writableStream.write(textAreaValue);
+        const json = JSON.parse(textAreaValue);
+        await writableStream.write(JSON.stringify(json, null, 2));
         await writableStream.close();
+
+        toast.success("Файл успешно сохранен!", {
+          position: "top-center",
+        });
       }
     } catch (error) {
       console.error("Error saving file:", error);
+
+      toast.error("Ошибка при сохранении файла!", {
+        position: "top-center",
+      });
     }
   };
 
   const inputHandler = (e) => {
     setTextAreaValue(e.target.value);
   };
+
+  const decodeJson = () => {
+    try {
+      setTextAreaValue('');
+      const json = JSON.parse(textAreaValue);
+      setFormattedJson(json);
+      setTextAreaValue(JSON.stringify(json, null, 2));
+
+    } catch (error) {
+      console.error("Error decoding JSON:", error);
+
+      toast.error("Ошибка при декодировании JSON" + error, {
+        position: "top-center",
+      });
+    }
+  }
 
   return (
     <>
@@ -52,6 +82,14 @@ function TextArea() {
           value={textAreaValue}
           onChange={inputHandler}
         ></textarea>
+        {formattedJson && (
+          <div className="json-viewer">
+            <label className="label-text-area">JSON древо:</label>
+            <div className="json-content">
+              <JSONTree data={formattedJson} theme={{ scheme: 'dark', author: 'sublime' }} />
+            </div>
+          </div>
+        )}
         <div className="main-buttons-wrapper">
           <button className="buttons" onClick={() => setIsModalOpen(true)}>Get</button>
           <button className="buttons" onClick={saveASHandler}>
@@ -60,10 +98,11 @@ function TextArea() {
           <button className="buttons" onClick={TextAreaClear}>
             Clear
           </button>
-          <button className="buttons">Decode</button>
+          <button className="buttons" onClick={decodeJson}>Decode</button>
         </div>
       </div>
       <Modal isOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+      <ToastContainer theme="dark" />
     </>
   );
 }
