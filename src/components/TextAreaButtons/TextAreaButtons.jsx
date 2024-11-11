40b39fd87551c50b71ff19a777b9fa51be67a7b6
First_Encounter <needenergy27@gmail.com>
2024-11-12 00:20:54 +0700

    add tooltips

--- src/components/TextAreaButtons/TextAreaButtons.jsx
+++ src/components/TextAreaButtons/TextAreaButtons.jsx
@@ -5,10 +5,10 @@ import "./TextAreaButtons.css";
 const TextAreaButtons = ({ onGet, onSaveAs, onClear, onDecode }) => {
     return (
         <div className="main-buttons-wrapper">
-            <button className="buttons" onClick={onGet}>Get</button>
-            <button className="buttons" onClick={onSaveAs}>Save as</button>
-            <button className="buttons" onClick={onClear}>Clear</button>
-            <button className="buttons" onClick={onDecode}>Decode</button>
+            <button className="buttons" title="Вызвать меню уточнения запроса" onClick={onGet}>Get</button>
+            <button className="buttons" title="Сохранить содержимое поля на компьютер" onClick={onSaveAs}>Save as</button>
+            <button className="buttons" title="Очистить поле" onClick={onClear}>Clear</button>
+            <button className="buttons" title="Отформатировать содержимое без сохранения" onClick={onDecode}>Decode</button>
         </div>
     )
 }
