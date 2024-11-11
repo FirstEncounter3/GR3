e18838c7509cab63f731b620c670f845fb5a31f8
First_Encounter <needenergy27@gmail.com>
2024-11-12 00:20:23 +0700

    add classname

--- src/components/ModalStoreSelector/ModalStoreSelector.jsx
+++ src/components/ModalStoreSelector/ModalStoreSelector.jsx
@@ -6,7 +6,7 @@ const ModalStoreSelector = () => {
     return (
         <div className='store-choice-wrapper'>
             <label for="store">Торговая точка</label>
-            <select name="store" id="store">
+            <select name="store" id="store" className='select-input'>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
