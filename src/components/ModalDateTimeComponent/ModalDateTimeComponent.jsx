424610e72edfe46728115a9666964970cba35fd6
First_Encounter <needenergy27@gmail.com>
2024-11-12 00:12:59 +0700

    add styles and class, fix structure

--- src/components/ModalDateTimeComponent/ModalDateTimeComponent.jsx
+++ src/components/ModalDateTimeComponent/ModalDateTimeComponent.jsx
@@ -7,13 +7,17 @@ const ModalDateTimeComponent = () => {
         <div className='label-and-calendar-wrapper'>
             <div className='date-and-time'>
                 <label for="since-date">С даты и времени</label>
-                <input type="datetime-local" id="since-date"></input>
-                <input type="number" id="since-seconds" name="seconds" min="0" max="59" placeholder="00"></input>
+                <div className='datetime-wrapper'>
+                    <input type="datetime-local" id="since-date" className='select-input-date'></input>
+                    <input type="number" id="since-seconds" name="seconds" min="0" max="59" placeholder="00" className='select-input-seconds'></input>
+                </div>
             </div>
             <div className='date-and-time'>
                 <label for="until-date">По дату и время</label>
-                <input type="datetime-local" id="until-date"></input>
-                <input type="number" id="until-seconds" name="seconds" min="0" max="59" placeholder="00"></input>
+                <div className='datetime-wrapper'>
+                    <input type="datetime-local" id="until-date" className='select-input-date'></input>
+                    <input type="number" id="until-seconds" name="seconds" min="0" max="59" placeholder="00" className='select-input-seconds'></input>
+                </div>
             </div>
         </div>
     )
