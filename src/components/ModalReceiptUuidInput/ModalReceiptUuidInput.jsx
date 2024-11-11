11603544dd215ac7330a8581a4409eb8aec55243
First_Encounter <needenergy27@gmail.com>
2024-11-12 00:19:14 +0700

    add placeholder

--- src/components/ModalReceiptUuidInput/ModalReceiptUuidInput.jsx
+++ src/components/ModalReceiptUuidInput/ModalReceiptUuidInput.jsx
@@ -6,7 +6,7 @@ const ModalReceiptUuidInput = () => {
     return (
         <div className='receipt-uuid-input-wrapper'>
             <label for="receipt-uuid">UUID чека</label>
-            <input type="text" id="receipt-uuid"></input>
+            <input type="text" placeholder='00000000-0000-0000-0000-000000000000' id="receipt-uuid" className='select-input'></input>
         </div>
     )
 }
